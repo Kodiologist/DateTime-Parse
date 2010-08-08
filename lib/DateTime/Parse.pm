c954a9d3cb4016e048dba1b19e66b532130c1921
Kodi Arfer <hippo@Thoth.(none)>
2010-08-08 09:42:23 -0500

    [Parse.pm] Rewrote to use grammar actions.

--- lib/DateTime/Parse.pm
+++ lib/DateTime/Parse.pm
@@ -1,6 +1,8 @@
 module DateTime::Parse {
 
-
+# ---------------------------------------------------------------
+# Data
+# ---------------------------------------------------------------
 
 my %months =
     <jan feb mar apr may jun jul aug sep oct nov dec> Z 1 .. 12;
@@ -11,22 +13,55 @@ my %dows =
 my %special-names = yes => -1, tod => 0, tom => 1;
   # Yesterday, today, and tomorrow.
 
-grammar StrDate {
+# ---------------------------------------------------------------
+# Internal functions
+# ---------------------------------------------------------------
 
-    token date { ^ [
+sub in (Str $haystack, Str $needle) {
+    $haystack.index($needle).defined
+}
+
+sub bm($b, $x) { $b ?? -$x !! $x }
+
+sub next-with-dow(Date $date, Int $dow, $backwards?) {
+# Finds the nearest date with day of week $dow that's
+#   later than $date     if $backwards is false   and
+#   earlier than $date   if $backwards is true.
+    $date + bm $backwards,
+        bm($backwards, (7 + $dow - $date.day-of-week.Int)) % 7 || 7
+}
+
+# ----------------------------------------------------------------
+# Grammar and actions
+# ----------------------------------------------------------------
+
+grammar DateTime::Parse::G {
+
+    regex date_only { ^ <date> $ }
+
+    token date {
         <special>                                         ||
         <next_last> <sep> <weekish>                       ||
         <weekish>   [<sep> <after_before>]?               ||
         [<dow> <sep>]?   [<ymd> || <md>]   [<sep> <dow>]?
-    ] $ }
+    }
     
     token special { <specialname> <alpha>* }
-
     token specialname { yes || tod || tom }
 
+    token weekish { week || <dow> }
+    token dow { <downame> <alpha>* }
+    token downame { mo || tu || we || th || fr || sa || su }
+
+    token next_last { next || last }
+    token after_before { after <sep> next || before <sep> last }
+    
     token ymd {
-         [$<y> = <yyyy>] <sep> <md> ||
-         <md>            <sep> <y>
+         <yyyy> <sep> <an> <sep> <an> ||
+           # A special case because the <an>s have to be
+           # interpreted as (month, day) regardless of :mdy.
+         <yyyy> <sep> <md>            ||
+         <md>   <sep> <y>
     }
 
     token md {
@@ -53,101 +88,106 @@ grammar StrDate {
     token an { \d\d? }
       # Ambiguous number.
 
-    token weekish { week || <dow> }
-    token dow { <downame> <alpha>* }
-    token downame { mo || tu || we || th || fr || sa || su }
-
-    token next_last { next || last }
-    token after_before { after <sep> next || before <sep> last }
-    
     token sep { <- alpha - digit>* }
 
 }
 
-sub bm($b, $x) { $b ?? -$x !! $x }
-# bm($b, $x) is a shortcut for ($b ?? -1 !! 1) * $x
+class DateTime::Parse::G::Actions {
+    has Date $.today = Date.today;
+    has Int $.yy-center = $.today.year;
+        # The year used to resolve two-digit year specifications.
+    has Bool $.mdy;
+    has Bool $.past;
+    has Bool $.future;
 
-sub next-with-dow(Date $date, Int $dow, Bool $backwards?) {
-# Finds the nearest date with day of week $dow that's
-#   later than $date     if $backwards is false   and
-#   earlier than $date   if $backwards is true.
-    $date + bm $backwards,
-        bm($backwards, (7 + $dow - $date.day-of-week.Int)) % 7 || 7
-}
+    method new(*%_) {
+        %_<past> and %_<future>
+            and fail "DateTime::Parse: You can't specify both :past and :future";
+        self.bless: *, |%_;
+    }
 
-our sub parse-date(
-        Str $s,
-        Date :$today = Date.today,
-        Int :$yy-center = $today.year,
-          # The year used to resolve two-digit year specifications.
-        Bool :$mdy,
-        Bool :$past is copy, Bool :$future is copy,
-    ) is export {
+    method date($/) {
+        
+        if $<special> {
 
-    $past and $future
-        and fail "DateTime::Parse::parse-date: You can't specify both :past and :future";
+            make $<special>.ast;
 
-    my $match = StrDate.parse(lc($s), :rule('date'))
-        or fail "DateTime::Parse::parse-date: No parse: $s";
-
-    if $match<special> {
-      # "Today" and the like.
-
-        $today + %special-names{~$match<special><specialname>}
-
-    } elsif $match<weekish> {
-      # "Monday" or "Tuesday before last" or "next week".
-
-        if ~ do $match<next_last> or $match<after_before> -> $s {
-           $match<weekish> eq 'week' and return $today +
-               bm $s ~~ /last/, do
-               $match<after_before> ?? 14 !! 7;
-           $past = ? do $s ~~ /last/;
-        };
-        ($match<after_before> ?? bm($past, 7) !! 0) +
-            next-with-dow $today,
-            %dows{~$match<weekish><dow><downame>},
-            $past;
-
-    } else {
-       # "1994/07/03" or "5th December".
+        } elsif $<weekish> {
+          # "Monday" or "Tuesday before last" or "next week".
     
-        my $md = $match<md> || $match<ymd><md>;
-
-        my ($month, $day);
-        if $md<an> {
-            # Ensure that "2010/12/31" is interpreted correctly
-            # even when !$mdy.
-            $mdy || $match<ymd> && $match<ymd><y>.from < $md.from
-                ?? ($month, $day) !! ($day, $month) =
-                    map +*, @($md<an>);
-        }
-        else {
-            $month = ~ do $md<m> || $md<mname>;
-            $month ~~ /<alpha>**3/ and $month = %months{~$/};
-            $day = +($md<d> || $md<dth>)[0];
-        }
+            if ~ do $<next_last> or $<after_before> -> $s {
+               $!past = in $s, 'last';
+               $<weekish> eq 'week' and return make $.today +
+                   bm $.past, $<after_before> ?? 14 !! 7;
+            }
+            make ($<after_before> ?? bm($.past, 7) !! 0) +
+                next-with-dow $.today,
+                %dows{~$<weekish><dow><downame>},
+                $.past;
     
-        my $year;
-        if $match<ymd> {
-            $year = $match<ymd><y>;
-            chars($year) == 2 and $year = min
-                map({ $^n - $^n % 100 + $year },
-                    $yy-center <<+<< (-100, 0, 100)),
-                by => { abs $^n - $yy-center };
-        } else {
-            my $ty = $today.year;
+        } elsif $<ymd> {
+
+           make $<ymd>.ast;
+
+        } elsif $<md> {
+
+            my ($month, $day) = @($<md>.ast);
+            my $ty = $.today.year;
             my $then = Date.new: $ty, $month, $day;
-            $year =
-                   $past   ?? $ty - ($then > $today)
-               !!  $future ?? $ty + ($then < $today)
+            my $year =
+                   $.past   ?? $ty - ($then > $.today)
+               !!  $.future ?? $ty + ($then < $.today)
                !!  min ($ty + 1, $ty, $ty - 1), by =>
-                       { abs $today - Date.new: $^n, $month, $day }
-        }
+                       { abs $.today - Date.new: $^n, $month, $day }        
+            make Date.new: +$year, $month, $day;
     
-        Date.new(+$year, $month, $day);
+        }
 
     }
+
+    method special($/) {
+    # "Today" and the like.
+        make $.today + %special-names{~$<specialname>}
+    }
+
+    method ymd($/) {
+    # "1994/07/03"
+        my $year = + do $<yyyy> || $<y>;
+        my ($month, $day) = $<an>
+         ?? (+$<an>[0], +$<an>[1])
+         !! @($<md>.ast);
+        # Handle two-digit years.
+        chars($year) == 2 and $year = min
+           map({ $^n - $^n % 100 + $year },
+               $.yy-center <<+<< (-100, 0, 100)),
+           by => { abs $^n - $.yy-center };
+        make Date.new: $year, $month, $day;
+    }
+
+    method md($/) {
+    # "5th December"
+        make $<an>
+          ?? $.mdy
+             ?? (+$<an>[0], +$<an>[1])
+             !! (+$<an>[1], +$<an>[0])
+          !! (($<m> || $<mname>).ast, +($<d> || $<dth>)[0])
+    }
+
+    method m($/) { make $<mname> ?? $<mname>.ast !! ~$/ }
+
+    method mname($/) { make %months{~$<mon3>} }
+
+}
+
+# ----------------------------------------------------------------
+# Exported functions
+# ----------------------------------------------------------------
+
+our sub parse-date(Str $s, *%_) is export {
+    my $actions = DateTime::Parse::G::Actions.new: |%_;
+    my $match = DateTime::Parse::G.parse(lc($s), :rule('date_only'), :actions($actions))
+        or die "DateTime::Parse::parse-date: No parse: $s";
+    $match<date>.ast;
 }
 
 
